40f6763742a22ce9d7efdeea59ca6402304f25d5
Gabriel Frigo <gabriel.frigo4@gmail.com>
2026-07-04 01:10:35 -0300

    Revision Shell

--- target/windows/environment.sh
+++ target/windows/environment.sh
@@ -10,33 +10,3 @@ export C_INCLUDE_PATH="$(cygpath -m /usr/local/include)"
 export CPLUS_INCLUDE_PATH="$(cygpath -m /usr/local/include)"
 export LIBRARY_PATH="$(cygpath -m /usr/local/lib)"
 export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig"
-
-### ================================
-### WINDOWS FUNCTIONS
-### ================================
-
-### --------------------------------
-### Manual
-### --------------------------------
-win-man() {
-	start "https://learn.microsoft.com/en-us/search/?terms=${1}"
-}
-
-### ================================
-### UNIX FUNCTIONS
-### ================================
-
-### --------------------------------
-### Manual
-### --------------------------------
-unix-man() {
-	section="${1}"
-	command="${2}"
-	number="$section"
-
-	if [[ ! "$section" =~ [0-9]$ ]]; then
-		number="${section%?}"
-	fi
-
-	w3m "https://www.man7.org/linux/man-pages/man$number/$command.$section.html"
-}
