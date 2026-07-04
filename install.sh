daebaa116a5bfee9748a107717bb89bf3a7bd946
Gabriel Frigo <gabriel.frigo4@gmail.com>
2026-07-04 01:27:35 -0300

    Revision Shell

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ esac
 ### --------------------------------
 ### Detect OS
 ### --------------------------------
-. "${SHELL_REPO_DIR}/core/detect.sh"
+. "${SHELL_REPO_DIR}/library/detect.sh"
 OS_NAME="$(detect_os)"
 
 ### --------------------------------
@@ -157,10 +157,6 @@ esac
 
 REPO_DIR_LINE="export SHELL_REPO_DIR=\"${SHELL_REPO_DIR}\""
 CONTEXT_LINE="export SHELL_CONTEXT=\"${SHELL_CONTEXT}\""
-CORE_DETECT_LINE="${SOURCE_CMD} \"\${SHELL_REPO_DIR}/core/detect.sh\""
-CORE_ENV_LINE="${SOURCE_CMD} \"\${SHELL_REPO_DIR}/core/environment.sh\""
-CORE_FUNC_LINE="${SOURCE_CMD} \"\${SHELL_REPO_DIR}/core/functions.sh\""
-CORE_VAULT_LINE="${SOURCE_CMD} \"\${SHELL_REPO_DIR}/core/vault.sh\""
 SOURCE_LINE="${SOURCE_CMD} \"\${SHELL_REPO_DIR}/target/${OS_NAME}/${SHELL_NAME}/prompt.sh\""
 SETUP_BLOCK="$(cat << EOF
 
@@ -174,10 +170,11 @@ SETUP_BLOCK="$(cat << EOF
 # ╰──────────────────────────────────────────────────────────╯
 ${REPO_DIR_LINE}
 ${CONTEXT_LINE}
-${CORE_DETECT_LINE}
-${CORE_ENV_LINE}
-${CORE_FUNC_LINE}
-${CORE_VAULT_LINE}
+
+for _f in "\${SHELL_REPO_DIR}/library/"*.sh; do [ -f "\${_f}" ] && ${SOURCE_CMD} "\${_f}"; done
+for _f in "\${SHELL_REPO_DIR}/core/"*.sh; do [ -f "\${_f}" ] && ${SOURCE_CMD} "\${_f}"; done
+unset _f
+
 ${SOURCE_LINE}
 EOF
 )"
